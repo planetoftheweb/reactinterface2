10ca60bd349e5417845d0e55f82ddf47e4d37ee2
Ray Villalobos <ray@planetoftheweb.com>
2018-12-13 19:15:10 -0800

    fetch API

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      myName: 'Ray'
+      myAppointments: []
     };
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
   }
 
   render() {
@@ -20,7 +33,6 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                {this.state.myName}
                 <AddAppointments />
                 <SearchAppointments />
                 <ListAppointments />
