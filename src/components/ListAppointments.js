73630838f46f29775e257ed954b9d111615ba82d
Ray Villalobos <ray@planetoftheweb.com>
2018-12-14 17:23:21 -0800

    using pre-built components

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import Moment from 'react-moment';
 
 class ListAppointments extends Component {
   render() {
@@ -7,13 +9,21 @@ class ListAppointments extends Component {
         {this.props.appointments.map(item => (
           <div className="pet-item col media py-3" key={item.aptId}>
             <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger">X</button>
+              <button className="pet-delete btn btn-sm btn-danger">
+                <FaTimes />
+              </button>
             </div>
 
             <div className="pet-info media-body">
               <div className="pet-head d-flex">
                 <span className="pet-name">{item.petName}</span>
-                <span className="apt-date ml-auto">{item.aptDate}</span>
+                <span className="apt-date ml-auto">
+                  <Moment
+                    date={item.aptDate}
+                    parse="YYYY-MM-dd hh:mm"
+                    format="MMM-d h:mma"
+                  />
+                </span>
               </div>
 
               <div className="owner-name">
