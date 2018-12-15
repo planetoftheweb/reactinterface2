74843e965352a020471f318cf214e69a63fd15e5
Ray Villalobos <ray@planetoftheweb.com>
2018-12-14 16:39:28 -0800

    list appointments with template

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 
 class ListAppointments extends Component {
   render() {
-    const listItems = this.props.appointments.map(item => (
-      <div>
-        <div>{item.petName}</div>
-        <div>{item.ownerName}</div>
-      </div>
-    ));
+    return (
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(item => (
+          <div className="pet-item col media py-3">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">X</button>
+            </div>
 
-    return <div>{listItems}</div>;
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">{item.aptDate}</span>
+              </div>
+
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span>{item.ownerName}</span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
