7dd7cb7fe72f06d27668b0b0434ed358b64ae2b8
Ray Villalobos <ray@planetoftheweb.com>
2018-12-19 10:48:55 -0500

    finished course

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { findIndex, without } from 'lodash';
 
 class App extends Component {
   constructor() {
@@ -23,6 +23,7 @@ class App extends Component {
     this.addAppointment = this.addAppointment.bind(this);
     this.changeOrder = this.changeOrder.bind(this);
     this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
 
   toggleForm() {
@@ -42,6 +43,16 @@ class App extends Component {
     });
   }
 
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
   addAppointment(apt) {
     let tempApts = this.state.myAppointments;
     apt.aptId = this.state.lastIndex;
@@ -130,6 +141,7 @@ class App extends Component {
                 <ListAppointments
                   appointments={filteredApts}
                   deleteAppointment={this.deleteAppointment}
+                  updateInfo={this.updateInfo}
                 />
               </div>
             </div>
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -19,7 +19,21 @@ class ListAppointments extends Component {
 
             <div className="pet-info media-body">
               <div className="pet-head d-flex">
-                <span className="pet-name">{item.petName}</span>
+                <span
+                  className="pet-name"
+                  contentEditable="true"
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      'petName',
+                      e.target.innerText,
+                      item.aptId
+                    )
+                  }
+                >
+                  {item.aptId}--
+                  {item.petName}
+                </span>
                 <span className="apt-date ml-auto">
                   <Moment
                     date={item.aptDate}
@@ -31,9 +45,34 @@ class ListAppointments extends Component {
 
               <div className="owner-name">
                 <span className="label-item">Owner: </span>
-                <span>{item.ownerName}</span>
+                <span
+                  contentEditable="true"
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      'ownerName',
+                      e.target.innerText,
+                      item.aptId
+                    )
+                  }
+                >
+                  {item.ownerName}
+                </span>
               </div>
-              <div className="apt-notes">{item.aptNotes}</div>
+              <div
+                className="apt-notes"
+                contentEditable="true"
+                suppressContentEditableWarning
+                onBlur={e =>
+                  this.props.updateInfo(
+                    'aptNotes',
+                    e.target.innerText,
+                    item.aptId
+                  )
+                }
+              >
+                {item.aptNotes}
+              </div>
             </div>
           </div>
         ))}
